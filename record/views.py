b46ef50c18cfaccc843ef2d7d4e2f8f376561916
PRINCEOYEBOLA <odeleyebola@gmail.com>
2021-12-24 06:21:26 +0100

    created a new app called record after deleting the first one

--- record/views.py
+++ record/views.py
@@ -1,9 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse('This is an application about record keeping')
-
-def about(request):
-    return HttpResponse('This is an application that helps in keeping data of members')
