445b36835c38287f3fbd671954505d2f7ade177c
PRINCEOYEBOLA <odeleyebola@gmail.com>
2021-12-27 07:42:41 +0100

    added fieldsets and fields

--- form/admin.py
+++ form/admin.py
@@ -6,8 +6,18 @@ from .models import State, Information
 class InformationAdmin(admin.ModelAdmin):
     list_display = ["first_name", "middle_name", "last_name", "date_of_birth", "date_updated", "date_created"]
     list_filter = ["first_name", "date_created"]
-    fieldset = (
+    fieldsets = (
+        ("Names", 
+        {"fields":("first_name", "middle_name", "last_name")}),
         
+        ("Others",
+         {
+           "fields":("state_of_origin", "date_of_birth", "summary")
+         })
+        
+                     
     )
+
+
 admin.site.register(State)
 #admin.site.register(Information, InformationAdmin)
--- form/models.py
+++ form/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class State(models.Model):
-    state = models.CharField(max_length = 20, null = False)
+    State = models.CharField(max_length = 20, null = False)
     
     def __str__(self):
         return f"{self.State}"
@@ -15,7 +15,10 @@ class Information(models.Model):
     state_of_origin = models.ForeignKey(State, null = False, on_delete = models.PROTECT)
     summary = models.TextField(null = True, help_text = "Tell us a little about yourself")
     date_of_birth = models.DateField(null = False)
-    date_created = models.DateField(auto_now = True)
-    date_updated = models.DateField(auto_now_add = True)
+    date_created = models.DateTimeField(auto_now = True)
+    date_updated = models.DateTimeField(auto_now_add = True)
+    
+    def __str__(self):
+        return f"{self.first_name} ({self.middle_name} {self.last_name} {self.date_of_birth}) ({self.date_created} {self.date_updated})"
     
     
