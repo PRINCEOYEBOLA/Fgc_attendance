ab7c190f8ff3794c953645139bbd8e4a20255b5d
PRINCEOYEBOLA <odeleyebola@gmail.com>
2021-12-27 07:02:19 +0100

    added @admin.register

--- form/admin.py
+++ form/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import State, Information
 
 # Register your models here.
+@admin.register(Information)
+class InformationAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "middle_name", "last_name", "date_of_birth", "date_updated", "date_created"]
+    list_filter = ["first_name", "date_created"]
+    fieldset = (
+        
+    )
+admin.site.register(State)
+#admin.site.register(Information, InformationAdmin)
